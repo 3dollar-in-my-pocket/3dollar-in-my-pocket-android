ed078f1c74b2e7973285b74a425fdf333a1beaaa
easy1234 <zion830@naver.com>
2020-10-14 17:57:33 +0900

    스플래시 화면 추가

---
+++ app/src/main/java/com/zion830/threedollars/splash/SplashActivity.kt
@@ -0,0 +1,47 @@
+package com.zion830.threedollars.splash
+
+import android.animation.Animator
+import android.content.Intent
+import androidx.activity.viewModels
+import com.zion830.threedollars.MainActivity
+import com.zion830.threedollars.R
+import com.zion830.threedollars.databinding.ActivitySplashBinding
+import com.zion830.threedollars.login.LoginActivity
+import com.zion830.threedollars.utils.SharedPrefUtils
+import zion830.com.common.base.BaseActivity
+
+class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>(R.layout.activity_splash) {
+
+    override val viewModel: SplashViewModel by viewModels()
+
+    override fun initView() {
+        binding.lottieView.playAnimation()
+        binding.lottieView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {
+                // do nothing
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                selectStartActivity()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                // do nothing
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+                // do nothing
+            }
+        })
+    }
+
+    fun selectStartActivity() {
+        startActivity(
+            when {
+                SharedPrefUtils.getAccessToken() == null -> Intent(this, LoginActivity::class.java)
+                else -> Intent(this, MainActivity::class.java)
+            }
+        )
+        finish()
+    }
+}
---
+++ app/src/main/java/com/zion830/threedollars/splash/SplashViewModel.kt
@@ -0,0 +1,6 @@
+package com.zion830.threedollars.splash
+
+import zion830.com.common.base.BaseViewModel
+
+class SplashViewModel : BaseViewModel() {
+}
