f54cc0e8c2919506b703336d0855818c693046ce
yunji <zion830@naver.com>
2021-01-26 02:50:47 +0900

    가게 등록시 카테고리와 아이콘이 맞지 않음 수정

--- app/src/main/java/com/zion830/threedollars/repository/model/MenuType.kt
+++ app/src/main/java/com/zion830/threedollars/repository/model/MenuType.kt
@@ -7,8 +7,8 @@ import com.zion830.threedollars.utils.StringUtils
 enum class MenuType(val key: String, @StringRes val displayNameId: Int) {
     BUNGEOPPANG("BUNGEOPPANG", R.string.bung),
     TAKOYAKI("TAKOYAKI", R.string.tako),
-    HOTTEOK("HOTTEOK", R.string.gye),
-    GYERANPPANG("GYERANPPANG", R.string.hodduck);
+    HOTTEOK("HOTTEOK", R.string.hodduck),
+    GYERANPPANG("GYERANPPANG", R.string.gye);
 
     fun getName() = StringUtils.getString(displayNameId)
 
