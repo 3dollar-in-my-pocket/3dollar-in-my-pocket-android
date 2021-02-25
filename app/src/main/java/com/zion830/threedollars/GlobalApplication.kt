38a3c68141b2e9b43187cddcf67b4d6808f6acc1
yunji <zion830@naver.com>
2021-02-25 19:45:17 +0900

    에드몹 설정 추가

--- app/src/main/java/com/zion830/threedollars/GlobalApplication.kt
+++ app/src/main/java/com/zion830/threedollars/GlobalApplication.kt
@@ -2,7 +2,9 @@ package com.zion830.threedollars
 
 import android.app.Application
 import android.content.Context
+import com.google.android.gms.ads.MobileAds
 import com.kakao.sdk.common.KakaoSdk
+import com.naver.maps.map.NaverMapSdk
 
 class GlobalApplication : Application() {
 
@@ -18,6 +20,9 @@ class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         APPLICATION_CONTEXT = applicationContext
+
         KakaoSdk.init(this, BuildConfig.KAKAO_KEY)
+        NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NMF_CLIENT_ID)
+        MobileAds.initialize(this)
     }
 }
