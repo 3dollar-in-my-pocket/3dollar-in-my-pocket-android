4928939643bcbf8c927e5b64afc5257427ccddb2
Mino <k3000js@gmail.com>
2022-11-21 17:00:54 +0900

    User Response 수정

--- app/src/main/java/com/zion830/threedollars/datasource/model/v2/response/my/User.kt
+++ app/src/main/java/com/zion830/threedollars/datasource/model/v2/response/my/User.kt
@@ -2,6 +2,8 @@ package com.zion830.threedollars.datasource.model.v2.response.my
 
 
 import com.google.gson.annotations.SerializedName
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import com.zion830.threedollars.datasource.model.LoginType
 
 data class User(
@@ -12,7 +14,16 @@ data class User(
     @SerializedName("userId")
     val userId: Int = 0,
     @SerializedName("medal")
-    val medal: Medal? = Medal()
+    val medal: Medal? = Medal(),
+    @Json(name = "marketingConsent")
+    val marketingConsent: String? = "",
+    @Json(name = "device")
+    val device: Device? = Device()
 ) {
+    @JsonClass(generateAdapter = true)
+    data class Device(
+        val isSetupNotification: Boolean = true
+    )
+
     fun isKakaoUser() = socialType == LoginType.KAKAO.socialName
 }
