f58338da6e5ad0922a8186ef55b2a9c657f1d86b
Mino <k3000js@gmail.com>
2022-11-24 14:20:44 +0900

    [fix] 한식 categoryId bulid type에 따라 구분

--- app/src/main/java/com/zion830/threedollars/datasource/model/v2/response/store/BossCategoriesResponse.kt
+++ app/src/main/java/com/zion830/threedollars/datasource/model/v2/response/store/BossCategoriesResponse.kt
@@ -2,6 +2,7 @@ package com.zion830.threedollars.datasource.model.v2.response.store
 
 
 import com.google.gson.annotations.SerializedName
+import com.zion830.threedollars.BuildConfig
 
 data class BossCategoriesResponse(
     @SerializedName("data")
@@ -13,12 +14,16 @@ data class BossCategoriesResponse(
 ) {
     data class BossCategoriesModel(
         @SerializedName("categoryId")
-        val categoryId: String? = "628a42eb899ec19e976e54d7",
+        val categoryId: String? = if (BuildConfig.DEBUG) {
+            "622b7d0105ecea5baeafd245"
+        } else {
+            "628a42eb899ec19e976e54d7"
+        },
         @SerializedName("name")
         val name: String = "한식",
         @SerializedName("imageUrl")
         val imageUrl: String? = "https://storage.threedollars.co.kr/menu/icon_menu_3x_bab.png",
         @SerializedName("description")
-        val description : String = "한식 만나기 30초 전"
+        val description: String = "한식 만나기 30초 전"
     )
 }
