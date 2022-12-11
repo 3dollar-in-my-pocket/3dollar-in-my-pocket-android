789a25fb24ebad8ca0e5a117b14a466bf13019ce
JJY <skwntnftkgk@gmail.com>
2022-12-11 17:09:28 +0900

    [Feat - 2] 비로그인시 로그인 다이얼로그 추가 TODO 로그인했을때 액션 정리안됨.

--- app/src/main/java/com/zion830/threedollars/ui/favorite/viewer/FavoriteViewerActivity.kt
+++ app/src/main/java/com/zion830/threedollars/ui/favorite/viewer/FavoriteViewerActivity.kt
@@ -3,9 +3,14 @@ package com.zion830.threedollars.ui.favorite.viewer
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.zion830.threedollars.BR
+import com.zion830.threedollars.Constants
+import com.zion830.threedollars.GlobalApplication
 import com.zion830.threedollars.R
 import com.zion830.threedollars.databinding.ActivityFavoriteViewerBinding
 import com.zion830.threedollars.datasource.model.v2.response.favorite.MyFavoriteFolderResponse
+import com.zion830.threedollars.ui.food_truck_store_detail.FoodTruckStoreDetailActivity
+import com.zion830.threedollars.ui.login.dialog.LoginRequestDialog
+import com.zion830.threedollars.ui.store_detail.StoreDetailActivity
 import dagger.hilt.android.AndroidEntryPoint
 import zion830.com.common.base.BaseActivity
 import zion830.com.common.ext.toStringDefault
@@ -34,7 +39,19 @@ class FavoriteViewerActivity : BaseActivity<ActivityFavoriteViewerBinding, Favor
         viewModel.eventClick.observe(this) {
             when (it) {
                 FavoriteViewerViewModel.Event.Close -> finish()
-                is FavoriteViewerViewModel.Event.Viewer -> {}
+                is FavoriteViewerViewModel.Event.Viewer -> {
+                    val item = it.item
+                    if (GlobalApplication.isLoggedIn) {
+                        val intent = if (item.storeType == Constants.BOSS_STORE) {
+                            FoodTruckStoreDetailActivity.getIntent(this@FavoriteViewerActivity, item.storeId)
+                        } else {
+                            StoreDetailActivity.getIntent(this@FavoriteViewerActivity, item.storeId.toInt())
+                        }
+                        startActivity(intent)
+                    } else {
+                        LoginRequestDialog().show(supportFragmentManager,"")
+                    }
+                }
             }
         }
         viewModel.favoriteViewer.observe(this) {
--- app/src/main/java/com/zion830/threedollars/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/zion830/threedollars/ui/mypage/MyPageFragment.kt
@@ -15,7 +15,6 @@ import com.zion830.threedollars.datasource.model.v2.response.favorite.MyFavorite
 import com.zion830.threedollars.datasource.model.v2.response.visit_history.VisitHistoryContent
 import com.zion830.threedollars.ui.MyPageSettingFragment
 import com.zion830.threedollars.ui.favorite.FavoriteMyFolderActivity
-import com.zion830.threedollars.ui.favorite.viewer.FavoriteViewerActivity
 import com.zion830.threedollars.ui.food_truck_store_detail.FoodTruckStoreDetailActivity
 import com.zion830.threedollars.ui.mypage.adapter.MyPageRecyclerAdapter
 import com.zion830.threedollars.ui.mypage.vm.MyPageViewModel
@@ -106,9 +105,7 @@ class MyPageFragment : BaseFragment<FragmentNewMyPageBinding, MyPageViewModel>(R
             )
         }
         binding.favoriteMoreTextView.setOnClickListener {
-            startActivity(Intent(requireActivity(), FavoriteViewerActivity::class.java).apply {
-                putExtra("favoriteId",viewModel.id)
-            })
+            startActivity(Intent(requireActivity(), FavoriteMyFolderActivity::class.java))
         }
         observeUiData()
     }
