e0370080b6f4e83fac237568d6d71205082e108c
Mino <k3000js@gmail.com>
2023-08-21 15:40:35 +0900

    [Feat] - 전체 메뉴 Apdater 추가

---
+++ app/src/main/java/com/zion830/threedollars/ui/category/adapter/SelectCategoryRecyclerAdapter.kt
@@ -0,0 +1,39 @@
+package com.zion830.threedollars.ui.category.adapter
+
+import android.app.Application
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.ListAdapter
+import com.zion830.threedollars.GlobalApplication
+import com.zion830.threedollars.R
+import com.zion830.threedollars.databinding.ItemCategoryBtnBinding
+import com.zion830.threedollars.datasource.model.v2.response.store.CategoriesModel
+import com.zion830.threedollars.ui.mypage.adapter.bindMenuIcon
+import zion830.com.common.base.BaseDiffUtilCallback
+import zion830.com.common.base.BaseViewHolder
+import zion830.com.common.base.loadUrlImg
+import zion830.com.common.listener.OnItemClickListener
+
+class SelectCategoryRecyclerAdapter(private val onClickListener: (CategoriesModel) -> Unit) :
+    ListAdapter<CategoriesModel, CategoryViewHolder>(BaseDiffUtilCallback()) {
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) = holder.bind(getItem(position), listener = null)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = CategoryViewHolder(parent, onClickListener)
+}
+
+class CategoryViewHolder(parent: ViewGroup, private val onClickListener: (CategoriesModel) -> Unit) :
+    BaseViewHolder<ItemCategoryBtnBinding, CategoriesModel>(R.layout.item_category_btn, parent) {
+
+    override fun bind(item: CategoriesModel, listener: OnItemClickListener<CategoriesModel>?) {
+        binding.run {
+            root.setOnClickListener { onClickListener(item) }
+            categoryImageView.loadUrlImg(item.imageUrl)
+            categoryNameTextView.text = item.name
+            newImageView.isVisible = item.isNew
+            selectCircleImageView.isVisible = item.isSelected
+            if (item.isSelected) {
+                categoryNameTextView.setTextColor(GlobalApplication.getContext().getColor(R.color.pink))
+            }
+        }
+    }
+}
