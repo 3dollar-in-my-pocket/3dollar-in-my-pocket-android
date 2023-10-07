adb4aa3c5eb33cc4e5d70a2efc3a3eb19a9f4a1d
JJY <skwntnftkgk@gmail.com>
2023-10-07 17:26:50 +0900

    [Feat] 투표화면 xml 완료

---
+++ community/presentation/src/main/java/com/threedollar/presentation/detail/CommunityDetailActivity.kt
@@ -0,0 +1,4 @@
+package com.threedollar.presentation.detail
+
+class CommunityDetailActivity {
+}
---
+++ community/presentation/src/main/java/com/threedollar/presentation/detail/CommunityDetailViewModel.kt
@@ -0,0 +1,6 @@
+package com.threedollar.presentation.detail
+
+import zion830.com.common.base.BaseViewModel
+
+class CommunityDetailViewModel :BaseViewModel(){
+}
