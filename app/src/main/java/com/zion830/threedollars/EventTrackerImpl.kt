fc9d05280ddbcf90f2afb5f35f38e3fb01327490
JJY <skwntnftkgk@gmail.com>
2023-12-23 19:09:57 +0900

    [Feat] 오류 수정

--- app/src/main/java/com/zion830/threedollars/EventTrackerImpl.kt
+++ app/src/main/java/com/zion830/threedollars/EventTrackerImpl.kt
@@ -2,10 +2,11 @@ package com.zion830.threedollars
 
 import android.os.Bundle
 import com.threedollar.common.listener.EventTrackerListener
+import javax.inject.Inject
 
-class EventTrackerImpl : EventTrackerListener {
+class EventTrackerImpl @Inject constructor() : EventTrackerListener {
 
-    override fun logEvent(name: String, params: Bundle? = null) {
+    override fun logEvent(name: String, params: Bundle?) {
         EventTracker.logEvent(name, params)
     }
 }
