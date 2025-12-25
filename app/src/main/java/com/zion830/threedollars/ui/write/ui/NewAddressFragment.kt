3355bbdb41590e37c9dc19d3c6325993e4f12e4c
정진용 <skwntnftkgk@gmail.com>
2025-12-25 18:00:22 +0900

    TH-933
    - 사장님 소개 노션페이지로 연결

--- app/src/main/java/com/zion830/threedollars/ui/write/ui/NewAddressFragment.kt
+++ app/src/main/java/com/zion830/threedollars/ui/write/ui/NewAddressFragment.kt
@@ -128,7 +128,7 @@ class NewAddressFragment : Fragment() {
                         BossDownloadDialog.newInstance()
                             .setOnConfirmListener {
                                 val intent = Intent(Intent.ACTION_VIEW).apply {
-                                    data = Uri.parse("market://details?id=app.threedollars.manager")
+                                    data = Uri.parse("https://massive-iguana-121.notion.site/3-28c7ad52990e809caba2fb2040677a2a?source=copy_link")
                                     setPackage("com.android.vending")
                                 }
                                 try {
@@ -137,7 +137,7 @@ class NewAddressFragment : Fragment() {
                                     startActivity(
                                         Intent(
                                             Intent.ACTION_VIEW,
-                                            Uri.parse("https://play.google.com/store/apps/details?id=app.threedollars.manager")
+                                            Uri.parse("https://massive-iguana-121.notion.site/3-28c7ad52990e809caba2fb2040677a2a?source=copy_link")
                                         )
                                     )
                                 }
@@ -421,160 +421,3 @@ fun BottomAddressSheet(
         }
     }
 }
-
-/*
-==================== 기존 코드 (블록 주석으로 보존) ====================
-
-package com.zion830.threedollars.ui.write.ui
-
-import android.content.Context
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
-import com.threedollar.common.base.BaseFragment
-import com.threedollar.common.utils.Constants
-import com.zion830.threedollars.EventTracker
-import com.zion830.threedollars.MainActivity
-import com.zion830.threedollars.R
-import com.zion830.threedollars.core.designsystem.R as DesignSystemR
-import com.zion830.threedollars.databinding.FragmentNewAddressBinding
-import com.zion830.threedollars.ui.dialog.NearExistDialog
-import com.zion830.threedollars.ui.map.ui.StoreAddNaverMapFragment
-import com.zion830.threedollars.ui.write.viewModel.AddStoreViewModel
-import com.zion830.threedollars.utils.NaverMapUtils.DEFAULT_DISTANCE_M
-import com.zion830.threedollars.utils.NaverMapUtils.calculateDistance
-import com.zion830.threedollars.utils.getCurrentLocationName
-import com.zion830.threedollars.utils.navigateSafe
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import zion830.com.common.base.onSingleClick
-import com.threedollar.common.R as CommonR
-
-@AndroidEntryPoint
-class NewAddressFragment : BaseFragment<FragmentNewAddressBinding, AddStoreViewModel>() {
-
-    override val viewModel: AddStoreViewModel by activityViewModels()
-
-    private lateinit var naverMapFragment: StoreAddNaverMapFragment
-
-    private lateinit var callback: OnBackPressedCallback
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                findNavController().navigate(R.id.navigation_home)
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
-        initNavigationBar()
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        callback.remove()
-    }
-
-    override fun initView() {
-        initMap()
-        initFlows()
-        initButton()
-    }
-
-    override fun initFirebaseAnalytics() {
-        setFirebaseAnalyticsLogEvent(className = "NewAddressFragment", screenName = "write_address")
-    }
-
-    private fun initNavigationBar() {
-        if (requireActivity() is MainActivity) {
-            (requireActivity() as MainActivity).showBottomNavigation(false)
-        }
-    }
-
-    private fun initButton() {
-        binding.backButton.onSingleClick {
-            findNavController().navigate(R.id.navigation_home)
-        }
-        binding.finishButton.onSingleClick {
-            val bundle = Bundle().apply {
-                putString("screen", "write_address")
-                putString("address", binding.addressTextView.text.toString())
-            }
-            EventTracker.logEvent(Constants.CLICK_SET_ADDRESS, bundle)
-            viewModel.selectedLocation.value?.let { location -> viewModel.getStoreNearExists(location) }
-        }
-    }
-
-    private fun initMap() {
-        naverMapFragment = StoreAddNaverMapFragment()
-        activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.mapContainer, naverMapFragment)?.commit()
-        naverMapFragment.setIsShowOverlay(false)
-    }
-
-    private fun initFlows() {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-                    viewModel.aroundStoreModels.collect { res ->
-                        naverMapFragment.addStoreMarkers(DesignSystemR.drawable.ic_mappin_focused_off, res ?: listOf())
-                    }
-                }
-                launch {
-                    viewModel.isNearStoreExist.collect {
-                        if (it) {
-                            showNearExistDialog()
-                        } else {
-                            val bundle = Bundle().apply {
-                                putString("screen", "write_address")
-                            }
-                            EventTracker.logEvent(Constants.CLICK_CURRENT_LOCATION, bundle)
-                            moveAddStoreDetailFragment()
-                        }
-                    }
-                }
-                launch {
-                    viewModel.selectedLocation.collect { latLng ->
-                        if (latLng != null) {
-                            binding.addressTextView.text = getCurrentLocationName(latLng) ?: getString(CommonR.string.location_no_address)
-                            val northWest = naverMapFragment.naverMap?.contentBounds?.northWest
-                            val southEast = naverMapFragment.naverMap?.contentBounds?.southEast
-                            viewModel.requestStoreInfo(
-                                latLng,
-                                if (northWest != null && southEast != null) calculateDistance(northWest, southEast).toDouble() else DEFAULT_DISTANCE_M
-                            )
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private fun showNearExistDialog() {
-        viewModel.selectedLocation.value?.let { location ->
-            NearExistDialog.getInstance(location.latitude, location.longitude)
-                .apply {
-                    setDialogListener(object : NearExistDialog.DialogListener {
-                        override fun accept() {
-                            moveAddStoreDetailFragment()
-                        }
-                    })
-                }.show(parentFragmentManager, NearExistDialog().tag)
-        }
-    }
-
-    private fun moveAddStoreDetailFragment() {
-        findNavController().navigateSafe(R.id.action_navigation_write_to_navigation_write_detail)
-    }
-
-    override fun getFragmentBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentNewAddressBinding =
-        FragmentNewAddressBinding.inflate(inflater, container, false)
-}
-
-==================== 기존 코드 끝 ====================
-*/
