914ce010b1b02d2cf9cc2ebd89f2380e640471ab
duitddu <du.it.ddu@gmail.com>
2026-01-16 20:53:05 +0900

    Update version code to 116

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         targetSdk = libs.versions.targetSdk.get().toInt()
 
         applicationId = "com.zion830.threedollars"
-        versionCode = 114
+        versionCode = 116
         versionName = project.findProperty("version_name") as String
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
