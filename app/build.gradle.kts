e80256ec96e607430b06f487f1b7e3e16f80382f
duitddu <du.it.ddu@gmail.com>
2026-04-20 00:35:19 +0900

    Update version to 4.18.0(121)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         targetSdk = libs.versions.targetSdk.get().toInt()
 
         applicationId = "com.zion830.threedollars"
-        versionCode = 120
+        versionCode = 121
         versionName = project.findProperty("version_name") as String
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
